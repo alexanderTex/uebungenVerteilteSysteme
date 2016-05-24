6a3beea57426426aefa4939375128e03f5ca1c82
aluedke <4t-kn0x.spm@gmx.de>
2016-05-24 17:00:00 +0200

    first version

--- ueb03_rmi/src/rmiClient/startRmiClient.java
+++ ueb03_rmi/src/rmiClient/startRmiClient.java
@@ -12,11 +12,16 @@ import rmiClassInterface.*;
 
 import java.io.*;
 import java.rmi.Naming;
-import java.util.Scanner;
 
 
 public class startRmiClient
 {
+    /**
+     * The RMI-Client
+     * @param args
+     *          [0] servername
+     *          [1] client nickname
+     */
     public static void main(String[] args)
     {
         // Check arguments for two inputs
@@ -25,107 +30,55 @@ public class startRmiClient
             System.out.println("The application needs two arguments: " + "<servername>" + "<nickname>");
         }
 
-        /*
-        InputStream inputStream = System.in;
-        //Reader inputStreamReader = new InputStreamReader(inputStream);
-        char c;
-        int i;
-
-        try {
-            while((i= inputStream.read())!=-1)
-            {
-                // converts integer to character
-                c = (char) i;
-                System.out.print(c);
-            }
-        }
-        catch (Exception e){}
-        */
-
         try
         {
             // args[0] == localhost; args[1] == alex
-            ChatClientImpl cc = new ChatClientImpl(args[1]);
-            ChatServer csi = (ChatServer) Naming.lookup("rmi://" + args[0] + "/rmiServer");
+            ChatClientImpl chatClientImpl = new ChatClientImpl(args[1]);
+            ChatServer chatServer = (ChatServer) Naming.lookup("rmi://" + args[0] + "/rmiServer");
 
-            // INFO
-            // System.out.println(csi.addClient(cc));
-            csi.addClient(cc);
+            chatServer.addClient(chatClientImpl);
 
-            // create scanner (input)
-            // input == choose write message(1) or exit(2)
-            //Scanner input = new Scanner(System.in);
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            boolean exit = false;
-            while (exit == false)
+            boolean exitApplication = false;
+            while (exitApplication == false)
             {
-                System.out.println("acitvity: \n 1) send message \n 2) exit");
-                System.out.print("activity: ");
-                Scanner input = new Scanner(System.in);
-                int activity = input.nextInt();
-                //input = null;
-                //input.close();
+                System.out.println("\n acitvity: \n 1) send message \n 2) exit");
+                System.out.print("choose activity: ");
+                String activity = bufferedReader.readLine();
 
                 switch (activity) {
 
-                    case 1: {
-                        //InputStream inputStream = System.in;
-                        //Reader inputStreamReader = new InputStreamReader(inputStream);
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-                        char c;
-                        int i;
-                        //String sendMsg = new String();
-
-                        Scanner sc = new Scanner(System.in);
-                        String sendMsg = "1";
-                        while (!sendMsg.equals("0"))
+                    case "1":
+                    {
+                        String inputBuffer = "1";
+                        while (!inputBuffer.equals("0"))
                         {
-                            try
+                            // Input message
+                            System.out.print("\nmsg: ");
+                            inputBuffer = bufferedReader.readLine();
+
+                            // No output for "0"
+                            if (!inputBuffer.equals("0"))
                             {
-                                InputStreamReader isr = new InputStreamReader(System.in);
-                                BufferedReader br = new BufferedReader(isr);
-                                System.out.print("msg: ");
-                                String eingabe = br.readLine();
-                                //Info
-                                System.out.println("Du hast " + eingabe + " eingegeben.");
+                                // Send message to server
+                                chatServer.sendMessage(args[1], inputBuffer);
                             }
-                            catch (IOException ioe){}
-
                         }
-                            /*
-                            // reset the input acitvity "1"
-                            sendMsg = "";
-
-                            try {
-                                while ((i = bufferedReader.read()) != 10) {
-                                    // converts integer to character
-                                    c = (char) i;
-                                    // System.out.print(i + " " + c + " ");
-                                    sendMsg += c;
-                                }
-                                    System.out.print(sendMsg);
-                                    csi.sendMessage("alex", sendMsg);
-                                    // reset for new message
-                                    sendMsg = "";
-
-                            } catch (Exception e) {
-                            }
-                            */
                         break;
                     }
 
-                    case 2: {
-                        exit = true;
-                        csi.removeClient(cc);
+                    case "2":
+                    {
+                        exitApplication = true;
+                        chatServer.removeClient(chatClientImpl);
                         System.out.println("exit");
-
-                        // close the scanner
-                        input.close();
-
                         break;
                     }
 
-                    default: {
+                    default:
+                    {
                         System.out.println("false activity");
                         break;
                     }
